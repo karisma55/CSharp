60017286784a66a5477309d2c4246a992d9424d5
Kamil Taktashev <karisma55@list.ru>
2023-04-27 23:32:46 +0500

    seminar1

---
+++ task2/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большшее, а какое меньшее.
+// a = 5; b = 7 -> max = 7
+// a = 2; b = 10 -> max = 10
+// a = -9; b = -3 -> max = -3
+
+Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a < b)    
+System.Console.WriteLine("Большее число: " + b + ", меньшее число: " + a);
+else System.Console.WriteLine($"Большее число: {b}, меньшее число: {a}");
+
+
+
+
+
+
+
+
---
+++ task7/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает последнюю цифру этого числа.
+// 456 -> 6
+// 782 -> 2
+// 918 -> 8
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int lastNumber = 0;
+
+if(number>99 && number < 1000)
+{
+   lastNumber = number % 10;
+   Console.Write($"Последняя цифра числа {number} равна {lastNumber}");
+}
+else
+{
+    Console.Write("Число не трехзначное");
+}
+
+
+
+
